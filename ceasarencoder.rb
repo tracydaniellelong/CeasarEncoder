1ea7f794a001ecbae39e833db96acc7a6ff4041d
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 15:51:25 -0400

    test returns number of elements in the string

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,3 +1,7 @@
 def encode(str)
+	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	str.length do |x|
+		p x
+	end
 
 end
