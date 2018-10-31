49a0f3a8ba8c00e63a6621c590be3ef1690a7e45
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 16:15:29 -0400

    returning empty string

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,9 +1,13 @@
 def encode(str)
+	x = 0
 	encoded = " "
-	to_be_encoded = str.split("")
+	to_be_encoded = str.downcase.split("")
 	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-	to_be_encoded.each do |x|
-		encoded << x
+	to_be_encoded.length.times do 
+		if to_be_encoded[x] == alphabet[x]
+			encoded << alphabet.shift(5)
+		end
+		x += 1
 	end
 	return encoded
 end
