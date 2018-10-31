8c68b97187bd06a2d6d1348e24cf8a6d111293d8
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 16:02:19 -0400

    returns original string

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,8 +1,9 @@
 def encode(str)
 	encoded = " "
+	to_be_encoded = str.split("")
 	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-	str.length do |x|
-		encode.push(x)
+	to_be_encoded.each do |x|
+		encoded << x
 	end
 	return encoded
 end
