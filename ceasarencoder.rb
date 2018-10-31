09a7f1a653dd48b55dc95cbbbd11ba64f57e1d42
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 15:53:38 -0400

    returns empty string

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,7 +1,8 @@
 def encode(str)
+	encoded = " "
 	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 	str.length do |x|
-		p x
+		encode.push(x)
 	end
-
+	return encoded
 end
