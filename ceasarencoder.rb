05a260ce5bbf807de594377dbf8cc3c2acae4f66
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 20:34:56 -0400

    used a comma to split the alphabet rotation back to a and it worked

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,10 +1,5 @@
 def encode(str)
-	encoded = " "
-	
-	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-	
-	encoded << str.tr('a-w', 'e-z')
-		
-	
+	encoded = ""
+	encoded << str.tr('a-v, w-z', 'e-z, a-d')	
 	return encoded
 end
