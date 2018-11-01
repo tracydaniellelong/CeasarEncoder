7e9786cb9dc2dbfae632b5ed1761bf153ed8abe8
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-01 19:22:51 -0400

    made decoder work

--- ceasardecoder.rb
+++ ceasardecoder.rb
@@ -1,6 +1,9 @@
 def decode(str)
 	decoded = ""
-
-
+	if str.class != String
+		decoded = "Enter a string"
+	elsif
+		decoded << str.downcase.tr('e-z, a-d, 4-9, 0-3', 'a-v, w-z, 0-5, 6-9')
+	end	
 	return decoded
 end
