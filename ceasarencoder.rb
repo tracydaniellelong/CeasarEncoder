c8cf2f3fa356346e5f7fc561cbd043f2d43c8b3a
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-01 19:33:32 -0400

    finished decoder

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -3,7 +3,8 @@ def encode(str)
 	if str.class != String
 		encoded = "Enter a string"
 	elsif
-	encoded << str.downcase.tr('a-v, w-z, 0-5, 6-9', 'e-z, a-d, 4-9, 0-3')
+		encoded << str.downcase.tr('a-v, w-z, 0-5, 6-9', 'e-z, a-d, 4-9, 0-3')
 	end	
 	return encoded
 end
+
--- testceasardecoder.rb
+++ testceasardecoder.rb
@@ -5,4 +5,25 @@ class Testing < Minitest::Test
 	def test_decode_xvegc
 		assert_equal("tracy", decode("xvegc"))
 	end
+	def test_decode
+		assert_equal("danger", decode("herkiv"))
+	end
+	def test_a_quote
+		assert_equal("to 'quote' her", decode("xs 'uysxi' liv"))
+	end
+	def test_a_sentence
+		assert_equal("my dog ate my code.", decode("qc hsk exi qc gshi."))
+	end
+	def test_with_numbers
+		assert_equal("it is 8:52 pm.", decode("mx mw 2:96 tq."))
+	end
+	def test_if_not_a_string
+		assert_equal("Enter a string", decode(6767))
+	end
+	def test_for_more_numbers
+		assert_equal("Enter a string", decode(59))
+	end
+	def test_for_symbols
+		assert_equal("*", decode("*"))
+	end
 end
