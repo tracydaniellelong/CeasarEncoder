598c276a39d687ae6dc7b0f7e194911a76b7d59f
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 20:55:06 -0400

    numbers arent changed

--- testceasarencoder.rb
+++ testceasarencoder.rb
@@ -14,4 +14,7 @@ class Testing < Minitest::Test
 	def test_a_sentence
 		assert_equal("qc hsk exi qc gshi.", encode("My dog ate my code."))
 	end
+	def test_with_numbers
+		assert_equal("mx mw 8:52 tq.", encode("It is 8:52 pm."))
+	end
 end
