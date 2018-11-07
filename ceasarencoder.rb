44ccb1e1fb9221abe483a1fbac5607ba8c1f2d55
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-06 20:41:17 -0500

    some letters are working some not

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,4 +1,6 @@
 def encode(str, day)
-	str.to_s.tr('a-u, v-z, A-U, V-Z, 0-5, 6-9', 'f-z, a-e, F-Z, A-E, 4-9, 0-3')
+	lower = 'a'.ord + day
+	upper = 'A'.ord + day
+	str.to_s.tr('A-Za-z, 0-5, 6-9', "#{upper.chr}-ZA-#{upper.chr}#{lower.chr}-za-#{lower.chr}, 4-9, 0-3")
 end
 
