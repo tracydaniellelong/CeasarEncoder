77bd2482aab9d7d220c8d503f732c2b6ca0e3524
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-06 20:44:41 -0500

    made tests pass

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,6 +1,8 @@
 def encode(str, day)
 	lower = 'a'.ord + day
+	endlower = lower - 1
 	upper = 'A'.ord + day
-	str.to_s.tr('A-Za-z, 0-5, 6-9', "#{upper.chr}-ZA-#{upper.chr}#{lower.chr}-za-#{lower.chr}, 4-9, 0-3")
+	endupper = upper -1
+	str.to_s.tr('A-Za-z, 0-5, 6-9', "#{upper.chr}-ZA-#{endupper.chr}#{lower.chr}-za-#{endlower.chr}, 4-9, 0-3")
 end
 
