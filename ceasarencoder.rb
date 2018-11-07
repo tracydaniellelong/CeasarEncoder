81bf1db15724c9dfb9f8438a6c273b86d25bbfb7
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-06 19:58:23 -0500

    made tests pass

--- ceasarencoder.rb
+++ ceasarencoder.rb
@@ -1,5 +1,4 @@
-def encode(str)
+def encode(str, day)
 	str.to_s.tr('a-u, v-z, A-U, V-Z, 0-5, 6-9', 'f-z, a-e, F-Z, A-E, 4-9, 0-3')
-
 end
 
--- testceasarencoder.rb
+++ testceasarencoder.rb
@@ -6,27 +6,27 @@ class Testing < Minitest::Test
 		assert_equal("ywfhd", encode("tracy", 5))
 	end
 	def test_encode_with_uppercase
-		assert_equal("IFSLJW", encode("DANGER"))
+		assert_equal("IFSLJW", encode("DANGER", 5))
 	end
 	def test_a_quote
-		assert_equal("Yt 'Vztyj' mjw", encode("To 'Quote' her"))
+		assert_equal("Yt 'Vztyj' mjw", encode("To 'Quote' her", 5))
 	end
 	def test_a_sentence
-		assert_equal("Rd itl fyj rd htij.", encode("My dog ate my code."))
+		assert_equal("Rd itl fyj rd htij.", encode("My dog ate my code.", 5))
 	end
 	def test_with_numbers
-		assert_equal("Ny nx 2:96 ur.", encode("It is 8:52 pm."))
+		assert_equal("Ny nx 2:96 ur.", encode("It is 8:52 pm.", 5))
 	end
 	def test_if_not_a_string
-		assert_equal("0101", encode(6767))
+		assert_equal("0101", encode(6767, 5))
 	end
 	def test_for_more_numbers
-		assert_equal("37", encode(93))
+		assert_equal("37", encode(93, 5))
 	end
 	def test_for_symbols
-		assert_equal("*", encode("*"))
+		assert_equal("*", encode("*", 5))
 	end
 	def test_for_symbols_2
-		assert_equal("@", encode("@"))
+		assert_equal("@", encode("@", 5))
 	end
 end
